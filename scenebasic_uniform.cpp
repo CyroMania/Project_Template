a431e0741e96d8145bb3c60dfa433cca313e5668
Toby Hodgkinson <tobyhodgkinson@gmail.com>
2023-03-25 20:54:04 +0000

    Setup the directional lights with their uniforms in 'scenebasic_uniform.cpp'.

--- scenebasic_uniform.cpp
+++ scenebasic_uniform.cpp
@@ -1,5 +1,5 @@
 #include "scenebasic_uniform.h"
-
+#include <sstream>
 #include <cstdio>
 #include <cstdlib>
 
@@ -18,28 +18,47 @@ using glm::vec3;
 using glm::mat4;
 
 //SceneBasic_Uniform::SceneBasic_Uniform() : torus(0.7f, 0.3f, 30, 30) {}
-SceneBasic_Uniform::SceneBasic_Uniform() : teapot(50, glm::translate(mat4(1.0f),vec3(0.0f, 0.0f, 0.0f))) {}
+SceneBasic_Uniform::SceneBasic_Uniform() : teapot(50, glm::translate(mat4(1.0f), vec3(0.0f, 0.0f, 0.0f))) {}
 
 void SceneBasic_Uniform::initScene()
 {
-    compile();
-    
-    glEnable(GL_DEPTH_TEST);
+	compile();
 
-    model = mat4(1.0f);
-    model = glm::rotate(model, glm::radians(270.0f), vec3(1.0f, 0.0f, 0.0f));
-    view = glm::lookAt(vec3(-1.0f, 4.0f, 5.0f), vec3(0.0f, 0.0f, 0.0f), vec3(0.0f, 0.1f, 0.0f));
+	glEnable(GL_DEPTH_TEST);
 
-    projection = mat4(1.0f);
-    
-    prog.setUniform("Material.Kd", vec3(0.2f, 0.5f, 0.8f));
-    prog.setUniform("Light.Ld", vec3(1.0f));
-    prog.setUniform("Light.Position", view * glm::vec4(5.0f, 5.0f, 2.0f, 1.0f));
-    prog.setUniform("Material.Ka", vec3(0.2f, 0.5f, 0.8f));
-    prog.setUniform("Light.La", vec3(0.4f));
-    prog.setUniform("Material.Ks", vec3(0.8f));
-    prog.setUniform("Light.Ls", vec3(1.0f));
-    prog.setUniform("Material.Shininess", 100.0f);
+	model = mat4(1.0f);
+	model = glm::rotate(model, glm::radians(270.0f), vec3(1.0f, 0.0f, 0.0f));
+	view = glm::lookAt(vec3(-1.0f, 5.0f, 5.0f), vec3(0.0f, 0.0f, 0.0f), vec3(0.0f, 0.1f, 0.0f));
+
+	projection = mat4(1.0f);
+	float x, z;
+	for (int i = 0; i < 3; i++)
+	{
+		std::stringstream name;
+		name << "Lights[" << i << "].Position";
+		x = 2.0f * cosf((glm::two_pi<float>() / 3) * i);
+		z = 2.0f * sinf((glm::two_pi<float>() / 3) * i);
+		prog.setUniform(name.str().c_str(), view * glm::vec4(x, 1.2f, z + 1.0f, 1.0f));
+	}
+
+	// 3 Directional Light Uniforms
+	prog.setUniform("Lights[0].La", vec3(0.0f, 0.0f, 0.1f));
+	prog.setUniform("Lights[1].La", vec3(0.0f, 0.1f, 0.0f));
+	prog.setUniform("Lights[2].La", vec3(0.1f, 0.0f, 0.0f));
+
+	prog.setUniform("Lights[0].Ld", vec3(0.0f, 0.0f, 1.8f));
+	prog.setUniform("Lights[1].Ld", vec3(0.0f, 1.8f, 0.0f));
+	prog.setUniform("Lights[2].Ld", vec3(1.8f, 0.0f, 0.0f));
+
+	prog.setUniform("Lights[0].Ls", vec3(0.0f, 0.0f, 0.8f));
+	prog.setUniform("Lights[1].Ls", vec3(0.0f, 0.8f, 0.0f));
+	prog.setUniform("Lights[2].Ls", vec3(0.8f, 0.0f, 0.0f));
+	//
+
+	prog.setUniform("Material.Kd", vec3(0.1f, 0.1f, 0.1f));
+	prog.setUniform("Material.Ka", vec3(0.9f, 0.9f, 0.9f));
+	prog.setUniform("Material.Ks", vec3(0.1f, 0.1f, 0.1f));
+	prog.setUniform("Material.Shininess", 180.0f);
 }
 
 void SceneBasic_Uniform::compile()
@@ -49,39 +68,40 @@ void SceneBasic_Uniform::compile()
 		prog.compileShader("shader/basic_uniform.frag");
 		prog.link();
 		prog.use();
-	} catch (GLSLProgramException &e) {
+	}
+	catch (GLSLProgramException& e) {
 		cerr << e.what() << endl;
 		exit(EXIT_FAILURE);
 	}
 }
 
-void SceneBasic_Uniform::update( float t )
+void SceneBasic_Uniform::update(float t)
 {
-    //model = glm::rotate(model, glm::radians(-1.0f), vec3(1.0f, 1.0f, 1.0f));
+	//model = glm::rotate(model, glm::radians(-1.0f), vec3(1.0f, 1.0f, 1.0f));
 }
 
 void SceneBasic_Uniform::render()
 {
-    GlCall(glClear(GL_COLOR_BUFFER_BIT));
-    GlCall(glClear(GL_DEPTH_BUFFER_BIT));
+	GlCall(glClear(GL_COLOR_BUFFER_BIT));
+	GlCall(glClear(GL_DEPTH_BUFFER_BIT));
 
-    setMatrices();
-    teapot.render(); 
+	setMatrices();
+	teapot.render();
 }
 
 void SceneBasic_Uniform::resize(int w, int h)
 {
-    glViewport(0, 0, w, h);
-    width = w;
-    height = h;
-    projection = glm::perspective(glm::radians(70.0f), (float)w / h,
-        0.3f, 100.0f);
+	glViewport(0, 0, w, h);
+	width = w;
+	height = h;
+	projection = glm::perspective(glm::radians(70.0f), (float)w / h,
+		0.3f, 100.0f);
 }
 
-void SceneBasic_Uniform::setMatrices() 
+void SceneBasic_Uniform::setMatrices()
 {
-    mat4 mv = view * model;
-    prog.setUniform("ModelViewMatrix", mv);
-    prog.setUniform("NormalMatrix", glm::mat3(vec3(mv[0]), vec3(mv[1]), vec3(mv[2])));
-    prog.setUniform("MVP", projection * mv);
+	mat4 mv = view * model;
+	prog.setUniform("ModelViewMatrix", mv);
+	prog.setUniform("NormalMatrix", glm::mat3(vec3(mv[0]), vec3(mv[1]), vec3(mv[2])));
+	prog.setUniform("MVP", projection * mv);
 }
